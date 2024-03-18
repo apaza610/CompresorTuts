d3fb7faee5201bc53970ee8c8ef6c90ad6568de7
apaza610 <homar.orozco@gmail.com>
2024-03-18 06:57:20 -0400

    Added checkbox to ask if user want to speed up/down the videos

---
+++ src/Hilo.java
@@ -0,0 +1,123 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Hilo extends Thread {
+    String pathRAIZ;
+    String currentMP4 = "";
+    JLabel lblDebug;
+    Stream<Path> lRecursiva;
+    List<Path> lPathsMP4s = new ArrayList<>();  // [D:\miFolder\deltaTime.mp4, D:\miFolder\interno\MiVideo.mp4]
+    Path[] lPathsTmp;   //se ira achicando con cada conversion
+    int operacion;
+    float RPDZ;
+
+    public Hilo(String cadena, JLabel lblDebug, int op, String rpdz){
+        pathRAIZ = cadena;
+        this.lblDebug = lblDebug;
+        this.operacion = op;
+        this.RPDZ = Float.parseFloat(rpdz);
+    }
+    @Override public void run(){
+        //********** ver si file lista.txt existe
+        File listaTXT = new File(pathRAIZ + "/lista.txt");
+
+        //********** lista.txt ∄ ⇒ crearlo y llenar de mp4 paths
+        if(!listaTXT.isFile()) {
+            try {
+                filtrarListaMP4s();
+                escribirEnTXT();
+                lblDebug.setText("lista.txt ha sido creada");
+            } catch (IOException e) { System.out.println("ha ocurrido un error"); }
+        }
+        //********** lista.txt ∃ leerlo y efectuar operaciones
+        else{
+            System.out.println("trabajando en el folder: " + pathRAIZ);
+            try {
+                leerListaTXT();
+            } catch (IOException e) { throw new RuntimeException(e); }
+            operarMP4s();
+            lblDebug.setText("fin: " + currentMP4);
+            listaTXT.delete();          //borrar cuando quede vacio
+            Toolkit.getDefaultToolkit().beep();
+        }
+    }
+
+    private void filtrarListaMP4s() throws IOException {
+        BiPredicate<Path, BasicFileAttributes> matcher = (elpath, atributo)-> String.valueOf(elpath).contains(".mp4");
+        lRecursiva = Files.find(Path.of(pathRAIZ), 3, matcher);
+        lPathsMP4s = lRecursiva.collect(Collectors.toList());
+        lPathsTmp = lPathsMP4s.toArray(new Path[0]);
+    }
+
+    private void escribirEnTXT() throws IOException {
+        FileWriter escribidor = new FileWriter(pathRAIZ + "/lista.txt");
+        for (Path x: lPathsTmp) {
+            try {
+                escribidor.write(x + "\n");
+            } catch (IOException e) { throw new RuntimeException(e); }
+        }
+        escribidor.close();
+    }
+
+    private void leerListaTXT() throws IOException {
+        lPathsMP4s.clear();
+        List<String> temp = Files.readAllLines(Paths.get("%s/lista.txt".formatted(pathRAIZ)));
+        temp.forEach(x-> lPathsMP4s.add(Paths.get(x)) );
+        lPathsTmp = lPathsMP4s.toArray(new Path[0]);
+    }
+
+    private void operarMP4s() {
+        System.out.println("--------------------------***-------------------------");
+        lPathsMP4s.forEach(x-> {
+            File file = new File(x.toUri());
+            currentMP4 = file.getName();
+            lblDebug.setText(currentMP4.length() > 30 ? currentMP4.substring(0,30) : currentMP4 );  //cortar pa que no empuje al boton
+            String xIN = file.getAbsolutePath();        //D:\miFolder\deltaTime.mp4
+            String xOUT = xIN.replace(".mp4","OUT.mp4");
+
+            Process proc = null;
+            try {
+                if(this.operacion == 1){
+                    proc = Runtime.getRuntime().exec(String.format("ffmpeg -i %s -vf scale=-1:720 -codec:v libx264 -codec:a aac -crf 25 -preset slow %s",xIN,xOUT));
+                } else if (this.operacion == 2) {
+                    proc = Runtime.getRuntime().exec(String.format("ffmpeg -i %s -vf \"setpts=%s*PTS\" -af \"atempo=%s\" %s",xIN,1/this.RPDZ,this.RPDZ,xOUT));
+                }
+                InputStream stderr = proc.getErrorStream();
+                InputStreamReader isr = new InputStreamReader(stderr);
+                BufferedReader bfrdr = new BufferedReader(isr);
+                String linea = null;
+                while ((linea = bfrdr.readLine()) != null) {
+                    System.out.println(linea);
+                }
+                int exitval = proc.waitFor();
+                System.out.println("valor exit: " + exitval);
+            } catch (Exception ex) { throw new RuntimeException(ex); }
+            file.delete();
+            renombrarFile(xOUT, xIN);
+            actualizarTXT();
+        });
+    }
+
+    private void actualizarTXT(){
+        lPathsTmp = Arrays.copyOfRange(lPathsTmp,1,lPathsTmp.length);
+        try {
+            escribirEnTXT();
+        } catch (IOException e) { throw new RuntimeException(e); }
+    }
+
+    private void renombrarFile(String fileOUT, String fileIN){
+        File f = new File(fileOUT);
+        f.renameTo(new File(fileIN));
+    }
+}
--- src/MiGUI.java
+++ src/MiGUI.java
@@ -1,120 +1,42 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.function.BiPredicate;
-import java.util.stream.Stream;
-import javax.swing.UIManager.LookAndFeelInfo;
 
 public class MiGUI extends JFrame{
     private JPanel pnlFondo;
-    private JTextArea taPathTutorial;
+    private JTextField tfPathTutorial;
     private JButton btnCompresion;
     private JLabel lblDebug;
-
+    private JTextField tfHayasa;
+    private JCheckBox checkBox;
     private Path pathRAIZ;       // D:\miFolder
-    private ArrayList<String> lDePathsMP4s = new ArrayList<>();
 
     public MiGUI(){
-        setTitle("ApzTool Compresor Tuts");
-        setSize(300, 100);
+        setTitle("Compresor MP4s");
+        setSize(360, 130);
         setVisible(true);
+        setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setContentPane(pnlFondo);
 
         btnCompresion.addActionListener( e -> {
-            pathRAIZ = Paths.get(taPathTutorial.getText());
-
-            //********** ver si file lista.txt existe
-            File listaTXT = new File(pathRAIZ + "/lista.txt");
-
-            //********** lista.txt ∄ ⇒ crearlo y llenar de mp4 paths
-            if(!listaTXT.isFile()){
-                try {
-                    filtrarListaMP4s();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+            pathRAIZ = Paths.get(tfPathTutorial.getText());
+            if (checkBox.isSelected()){
+                Hilo hilo1 = new Hilo(pathRAIZ.toString(), lblDebug, 2, tfHayasa.getText());
+                hilo1.start();
             }
-            //********** lista.txt ∃ leerlo y efectuar operaciones
             else{
-                try {
-                    leerListaTXT();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                Hilo hilo1 = new Hilo(pathRAIZ.toString(), lblDebug, 1, tfHayasa.getText());
+                hilo1.start();
             }
-
         } );
     }
 
-    private void filtrarListaMP4s() throws IOException {
-        //Path esteDirectorio = Paths.get("D:\\miFolder");
-
-        BiPredicate<Path, BasicFileAttributes> matcher = (elpath, atributo)-> String.valueOf(elpath).contains(".mp4");
-        Stream<Path> lCosas = Files.find(pathRAIZ, 2, matcher);
-//        lCosas.forEach(x -> System.out.println(x));
-        lCosas.forEach(x -> lDePathsMP4s.add(x.toString()));
-        //System.out.println(lDePathsMP4s);             //[D:\miFolder\deltaTime.mp4, D:\miFolder\interno\MiVideo.mp4, D:\miFolder\laMacaaarena.mp4]
-        guardarEnTXT();
-        lblDebug.setText("lista.txt ha sido creado");
-    }
-    private void guardarEnTXT() throws IOException {
-        FileWriter escribidor = new FileWriter(pathRAIZ + "/lista.txt");
-        lDePathsMP4s.forEach(x -> {
-            try {
-                escribidor.write(x + "\n");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        escribidor.close();
-    }
-    private void leerListaTXT() throws IOException {
-        lDePathsMP4s.clear();
-        lDePathsMP4s.addAll(Files.readAllLines(Paths.get(pathRAIZ + "/lista.txt")));
-
-        String[] lCadenas = lDePathsMP4s.toArray(new String[0]);
-        for (String cadena : lCadenas){
-            comprimirMP4(cadena);
-            lDePathsMP4s.remove(0);
-            guardarEnTXT();
-        }
-    }
-    private void comprimirMP4(String elMP4) throws IOException {
-        System.out.println("Operando sobre " + elMP4.substring(elMP4.lastIndexOf('\\')+1));         //Operando sobre D:\miFolder\laMacaaarena.mp4
-        lblDebug.setText(elMP4.substring(elMP4.lastIndexOf('\\')+1));
-        Runtime runtime = Runtime.getRuntime();
-
-        String argOUT= elMP4.replace(".mp4", "NEW.mp4");
-        runtime.exec(new String[]{"ffmpeg","-i",elMP4,"-vf","scale=-1:720","-codec:v","libx264","-codec:a","aac","-crf","25","-preset","slow",argOUT});
-
-//        Scanner s = new Scanner(System.in);
-//        s.next();             // pausa espera por keyboard
-    }
-
-    public static void main(String[] args) throws IOException {
-//        Predicate<? super Path> predicado = path -> String.valueOf(path).contains(".mp4");
-//        Stream<Path> lista = Files.walk(esteDirectorio, 1).filter(predicado);
-//        lista.forEach(System.out::println);
-
-//        Stream<Path> lista = Files.list(esteDirectorio);
-//        lista.forEach(System.out::println);
-
-        try {
-            for(LookAndFeelInfo info:UIManager.getInstalledLookAndFeels()){
-                if ("Nimbus".equals(info.getName())){  UIManager.setLookAndFeel(info.getClassName());  break; }
-            }
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
+    public static void main(String[] args) throws IOException, UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatDarculaLaf());
         MiGUI migui = new MiGUI();
     }
 }
